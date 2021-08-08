ecafe35724b274b0ee2879e62cf249613e34f924
inthemonzon <inthemonzon@yahoo.com>
2021-08-08 19:14:04 -0400

    commit tree d12f35fc70cbc458999dccd46f5af08a1b8cf1b9

---
+++ app.rb
@@ -0,0 +1,26 @@
+# app.rb 
+# puts = console.log()
+# = // (comment)
+
+puts "Hello World!"
+puts("Hello World!")
+#Line 5 and 6 do the same thing
+
+#puts and print do the same thing, puts just adds a line break
+puts "Hello world!"
+puts "Hello world!"
+puts "Hello world!"
+
+#puts and print are good for strings
+#p is good for arrays
+#pp is good for objects
+p [1, 2, 3]
+
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+#Using IRB is a great way to quickly test out some code, or check your syntax, without needing to run an entire application.
+#nil = null
+#puts always returns nil
+#exit IRB by typing exit or using cntrl + d
+
+print "Pass this test, please."
